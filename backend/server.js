b965f28162fe218b1b7a21f29916fe424a1f4cbf
anmolgoyal698 <anmolgoyal698@gmail.com>
2024-12-09 13:30:10 +0530

    connecting backend to mongodb database

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,28 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import products from './data/products.js';
+import express from "express";
+import dotenv from "dotenv";
+import products from "./data/products.js";
+import connectDB from "../config/db.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
+
+connectDB(); //Connect to MongoDB
+
 const app = express();
 
-app.get('/', (req, res)=> {
+app.get("/", (req, res) => {
   res.send("API is running");
-})
+});
 
-app.get('/api/products', (req, res) => {
+app.get("/api/products", (req, res) => {
   res.json(products);
-})
+});
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find(product => product._id === req.params.id);
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((product) => product._id === req.params.id);
   res.json(product);
-})
+});
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
-})
+});
